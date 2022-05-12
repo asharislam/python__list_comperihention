a84b87407c3cfae8dbd57415429fc3e509de0f9f
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2022-05-12 23:48:54 +0600

    Add files via upload

---
+++ list_comperihention.py
@@ -0,0 +1,31 @@
+# list comprehension
+#"Ijhar" 
+
+"""name_chars = []
+name = "Ashar"
+for char in name:
+    name_chars.append(char)
+    print(name_chars)"""
+
+
+    ##############
+"""name_chars = []
+name = "Ashar"
+for char in name:
+    name_chars.append(char)
+print(name_chars)
+"""
+#######short code ###########
+"""num_list = [number for number in range(30)]
+print(num_list)"""
+
+"""char_list = [ char for char in "ashar"]
+print(char_list)"""
+
+"""name = "majhar"
+char_list = [ char for char in name]
+print(char_list)"""
+
+############# even number print########
+even_number = [ even for even in range(2, 12, 2) ]
+print(even_number)
